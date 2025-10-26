f0c4ecf7862fd4583480882972b0b968da8c79c5
emre-akakce <emre@akakce.com>
2025-10-26 14:00:22 +0300

    add sample lib usage

---
+++ src/view/EmptyView.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button, ProductList } from '/Users/emrekardaslar/Desktop/Workspace/Self/component-library/dist/esm'
+function EmptyView() {
+    const products = [
+        { id: '1', name: 'Product A', price: 10.99 },
+        { id: '2', name: 'Product B', price: 20.50 },
+        { id: '3', name: 'Product C', price: 5.75 },
+    ]
+  return (
+    <>
+        <Button label="Click" />
+        <ProductList products={products} />
+    </>
+  )
+}
+
+export default EmptyView
