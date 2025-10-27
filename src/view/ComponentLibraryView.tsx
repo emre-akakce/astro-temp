2c6d8f2dd8656b35503b26b04d677f958e5de2ac
emre-akakce <emre@akakce.com>
2025-10-27 15:05:25 +0300

    rename empty view

--- src/view/ComponentLibraryView.tsx
+++ src/view/ComponentLibraryView.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button, ProductList } from '/Users/emrekardaslar/Desktop/Workspace/Self/component-library/dist/esm'
-function EmptyView() {
+function ComponentLibraryView() {
     const products = [
         { id: '1', name: 'Product A', price: 10.99 },
         { id: '2', name: 'Product B', price: 20.50 },
@@ -14,4 +14,4 @@ function EmptyView() {
   )
 }
 
-export default EmptyView
+export default ComponentLibraryView
