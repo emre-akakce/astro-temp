591ad05104c30f5d4a8024acdd90314230cd25fa
emre-akakce <emre@akakce.com>
2025-11-05 14:32:24 +0300

    move fetchProducts outside

--- src/viewmodel/product/useProductViewModel.ts
+++ src/viewmodel/product/useProductViewModel.ts
@@ -25,20 +25,21 @@ export const useProductViewModel = () => {
     }
   }, [eventMap]);
 
-  // Fetch products whenever the selected filter changes
-  useEffect(() => {
-    const fetchProducts = async () => {
-      dispatch(setProductsLoadingAction(true));
-      try {
-        const products = await getProducts(selectedFilter, language); // Use selectedFilter from filter viewmodel
-        dispatch(setProductsAction(products));
-      } catch (error) {
-        console.error('Failed to fetch products:', error);
-      } finally {
-        dispatch(setProductsLoadingAction(false));
-      }
-    };
+  const fetchProducts = async () => {
+    dispatch(setProductsLoadingAction(true));
+    try {
+      const products = await getProducts(selectedFilter, language); // Use selectedFilter from filter viewmodel
+      dispatch(setProductsAction(products));
+    } catch (error) {
+      console.error('Failed to fetch products:', error);
+    } finally {
+      dispatch(setProductsLoadingAction(false));
+    }
+  };
+  
 
+  // Fetch products whenever the selected filter changes
+  useEffect(() => {   
     if (selectedFilter) { // Only fetch if a filter is selected
       fetchProducts();
     }
